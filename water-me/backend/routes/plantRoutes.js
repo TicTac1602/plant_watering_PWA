1376e3229e514e64793de15fd058565a5d6d1b60
Emil Toulouse <emil.toulouse@epita.fr>
2024-03-24 00:12:49 +0100

    Added file compatibility

--- water-me/backend/routes/plantRoutes.js
+++ water-me/backend/routes/plantRoutes.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
-const authController = require('../controllers/plantController');
+const path = require('path');
+const imageDirectory = path.join(__dirname, '../database/uploads');
+const plantController = require('../controllers/plantController');
 
-router.get('/', authController.getAllPlants);
-router.get('/:id', authController.getOnePlant);
-router.get('/user/:id', authController.getAllPlantsByUser);
-router.post('/', authController.createPlant);
-router.put('/:id', authController.updatePlant);
-router.delete('/:id', authController.deletePlant);
+const multer = require('multer');
+
+const uploadDir = 'backend/database/uploads/';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir); // Spécifiez le dossier où vous souhaitez stocker les fichiers téléchargés
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Utilisez le nom de fichier original
+  }
+});
+
+const upload = multer({ storage: storage });
+
+
+router.get('/', plantController.getAllPlants);
+router.get('/:id', plantController.getOnePlant);
+router.get('/user/:id', plantController.getAllPlantsByUser);
+router.post('/', upload.single('photo'), plantController.createPlant);
+router.put('/:id',upload.single('photo'), plantController.updatePlant);
+router.delete('/:id', plantController.deletePlant);
+router.get('/images/:imageName', (req, res) => {
+  const imageName = req.params.imageName;
+  res.sendFile(path.join(imageDirectory, imageName));
+});
 
 module.exports = router;
